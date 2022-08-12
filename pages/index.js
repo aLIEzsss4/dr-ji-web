5e55ea77bff10dd3eedec6ebfc8ca48c29a4d05f
aLIEzsss4 <vuetomato@gmail.com>
2022-08-13 04:25:48 +0800

    feat singer gql

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const { Text } = Typography;
 // import { Network, Alchemy } from "alchemy-sdk";
 // import { gql } from "@apollo/client";
 import client from "../apollo-client";
-import {useState} from 'react'
+import { useState } from 'react'
 
 const QUERY1 = gql`
   query  {
@@ -53,15 +53,18 @@ const columns = [
     dataIndex: 'hold',
     key: 'hold',
     align: 'right',
+    width:80
+
   },
   {
-    title: '累计操作次数',
+    title: '操作次数',
     dataIndex: 'totalActions',
     key: 'totalActions',
     align: 'right',
+    width:80
   },
   {
-    title: '累计支出(E)',
+    title: '支出(E)',
     dataIndex: 'totalExpense',
     key: 'totalExpense',
     align: 'right',
@@ -74,34 +77,54 @@ const columns = [
   },
 
   {
-    title: '累计收入(E)',
+    title: <div>税前收入(E)</div>,
     dataIndex: 'totalIncome',
     key: 'totalIncome',
     align: 'right',
     // width: 80,
     render: (v, item) => {
-      return (v / 10 ** 18).toFixed(5)
+      return (v / 10 ** 18).toFixed(5) 
+    }
+  },
+  {
+    title: <div>税后收入(E)<Tooltip title="2.5%服务费+7.5%税费">？</Tooltip></div>,
+    dataIndex: 'totalIncome',
+    key: 'totalIncome2',
+    align: 'right',
+    // width: 80,
+    render: (v, item) => {
+      return (v / 10 ** 18) * 0.9.toFixed(5)
     }
   },
 
   {
-    title: '累计Gas(E)',
+    title: 'Gas(E)',
     dataIndex: 'totalGas',
     key: 'totalGas',
     align: 'right',
     // width: 80,
     render: (v, item) => {
-      return (v / 10 ** 18).toFixed(5)
+      return (v / 10 ** 13).toFixed(5)
     }
   },
   {
-    title: '当前收益(E)',
+    title: '税前收益(E)',
     dataIndex: 'currentProfits',
     key: 'currentProfits',
     align: 'right',
     // width: 80,
     render: (v, item) => {
-      return  (v / 10 ** 18).toFixed(5)
+      return (v / 10 ** 18).toFixed(5)
+    }
+  },
+  {
+    title: <div>税后收益(E)<Tooltip title="2.5%服务费+7.5%税费+Gas费">？</Tooltip></div>,
+    dataIndex: 'currentProfits',
+    key: 'currentProfits2',
+    align: 'right',
+    // width: 80,
+    render: (v, item) => {
+      return ((item.totalExpense - (item.totalIncome) * 0.9 - item.totalGas) / 10 ** 18).toFixed(5)
     }
   },
 
@@ -110,12 +133,51 @@ const columns = [
 
 export default function App(props) {
 
+  const QUERY_ID =  gql`
+  query  fn($id:ID!){
+    users(where: { owner: $id },first:1) {
+      id
+      totalActions
+      totalGas
+      totalIncome
+      totalExpense
+      currentProfits
+      hold
+    }
+  }
+`;
+
+
+
   const [value, setValue] = useState(1);
 
+  const [QUERY, setQUERY] = useState(QUERY1);
 
-  const QUERY = value == 1 ? QUERY1 :QUERY2
-  const { data, loading, error } = useQuery(QUERY);
+  const [addrs, setAddrs] = useState("");
 
+
+
+  const { data, loading, error, refetch } = useQuery(QUERY, {
+    variables:{
+      id: addrs
+    },
+
+  });
+
+  const onSearch = (value) => {
+    // console.log
+    // refetch({ query: QUERY_ID(value),id:33 })
+    setValue(0)
+    setAddrs(value)
+    setQUERY(
+      QUERY_ID,
+      {
+        variables:{
+          id:value
+        }
+      }
+      )
+  }
 
   if (loading) {
     return <h2>Loading...</h2>;
@@ -128,6 +190,8 @@ export default function App(props) {
 
   const onChange = (e) => {
     setValue(e.target.value);
+    const newQUERY = e.target.value == 1 ? QUERY1 : QUERY2
+    setQUERY(newQUERY)
   };
 
   // console.log(data,'data')
@@ -150,6 +214,11 @@ export default function App(props) {
             <Radio.Button value={1}>血亏榜</Radio.Button>
             <Radio.Button value={2}>盈利榜</Radio.Button>
           </Radio.Group>
+        </div>
+
+        <div className={styles.grid}>
+          <p>输入地址查询 </p>
+          <Search placeholder="Ethereum address " allowClear onSearch={onSearch} enterButton />
         </div>
 
         <Table key={value} size="small" columns={columns} style={{ width: 1000, marginTop: 40 }} scroll={{ x: 400, y: 400 }} dataSource={data.users}
